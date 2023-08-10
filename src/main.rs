61531995d032e6b1eef24ce72859ea4139f3d0f6
Shuhei YOSHIDA <contact@yantene.net>
2023-08-10 20:30:04 +0900

    feat: allow the tool to take an output path

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod compress;
 mod decompress;
 
-use clap::{parser::ValuesRef, Arg, Command};
+use clap::{Arg, Command};
 use compress::compress;
-use decompress::decompress;
 
 fn main() {
     let matches = Command::new("Kozuchi")
@@ -13,27 +12,39 @@ fn main() {
         .subcommand(
             Command::new("compress")
                 .about("Compress files")
-                .arg(Arg::new("file").required(true).num_args(1..)),
+                .arg(Arg::new("output").short('o'))
+                .arg(Arg::new("file").required(true).num_args(1)),
         )
         .subcommand(
             Command::new("decompress")
                 .about("Decompress files")
-                .arg(Arg::new("file").required(true).num_args(1..)),
+                .arg(Arg::new("output").short('o'))
+                .arg(Arg::new("file").required(true).num_args(1)),
         )
         .get_matches();
 
     match matches.subcommand() {
         Some(("compress", sub_m)) => {
-            let file_paths: ValuesRef<String> = sub_m.get_many("file").unwrap();
-            for file_path in file_paths {
-                compress(file_path);
-            }
+            let input_file_path = sub_m.get_one::<String>("file").unwrap();
+            let default_output_file_path = format!("{}.kozuchi", input_file_path);
+            let output_file_path = sub_m
+                .get_one::<String>("output")
+                .unwrap_or(&default_output_file_path);
+
+            compress(input_file_path, &output_file_path);
         }
         Some(("decompress", sub_m)) => {
-            let file_paths: ValuesRef<String> = sub_m.get_many("file").unwrap();
-            for file_path in file_paths {
-                decompress(file_path);
-            }
+            let input_file_path = sub_m.get_one::<String>("file").unwrap();
+            let default_output_file_path = if input_file_path.ends_with(".kozuchi") {
+                input_file_path.trim_end_matches(".kozuchi").to_string()
+            } else {
+                format!("{}.unkozuchied", input_file_path)
+            };
+            let output_file_path = sub_m
+                .get_one::<String>("output")
+                .unwrap_or(&default_output_file_path);
+
+            compress(input_file_path, &output_file_path);
         }
         _ => {}
     }
