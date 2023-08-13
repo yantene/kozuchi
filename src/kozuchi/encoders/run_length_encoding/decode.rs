006cb69184990f3128feadde5d2ee03ed6b7db4d
Shuhei YOSHIDA <contact@yantene.net>
2023-08-14 00:42:54 +0900

    feat: modularize run-length encoding implementation into encoder module

---
+++ src/kozuchi/encoders/run_length_encoding/decode.rs
@@ -0,0 +1,57 @@
+use std::{iter::Peekable, marker::PhantomData};
+
+fn take_block_run_lengths<'a>(input: &mut Peekable<impl Iterator<Item = u8> + 'a>) -> [u8; 256] {
+    let mut run_lengths_map = [0u8; 256];
+
+    while input.peek().is_some() && &0 != input.peek().unwrap() {
+        let run_length = input.next().unwrap();
+        let index = input.next().unwrap();
+
+        run_lengths_map[index as usize] = run_length;
+    }
+    input.next();
+
+    return run_lengths_map;
+}
+
+pub fn run<'a, I: Iterator<Item = u8> + 'a>(input: &'a mut I) -> impl Iterator<Item = u8> + 'a {
+    struct Decoded<'a, I: Iterator<Item = u8> + 'a> {
+        input: Peekable<I>,
+        current_chunk: Vec<u8>,
+        _marker: PhantomData<&'a mut I>,
+    }
+
+    impl<'a, I: Iterator<Item = u8>> Iterator for Decoded<'a, I> {
+        type Item = u8;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.current_chunk.len() == 0 {
+                if self.input.peek().is_none() {
+                    return None;
+                }
+
+                while self.input.peek().is_some() {
+                    let run_lengths_map = take_block_run_lengths(&mut self.input);
+
+                    for index in 0u8..=255u8 {
+                        let byte = match self.input.next() {
+                            Some(byte) => byte,
+                            None => break,
+                        };
+
+                        self.current_chunk
+                            .extend(vec![byte; run_lengths_map[index as usize] as usize + 1]);
+                    }
+                }
+            }
+
+            Some(self.current_chunk.remove(0))
+        }
+    }
+
+    Decoded {
+        input: input.peekable(),
+        current_chunk: Vec::new(),
+        _marker: PhantomData,
+    }
+}
